7bb8670cf098d779e380ca6baf83e31e8a04a674
Renato Augusto <107155217+Argustor@users.noreply.github.com>
2023-03-09 15:32:14 -0300

    Create distanciaCarros.java

---
+++ distanciaCarros.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[] args) {
+        int carroX, carroY, distancia;
+        carroX = 60;
+        carroY = 90;
+        distancia = 0;
+
+        Scanner myObj = new Scanner(System.in);
+        int km = myObj.nextInt();
+
+        distancia = km * 2;
+        System.out.println(distancia + " minutos");
+
+        }
+
+            
+    }
