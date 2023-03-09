52950504849cab8420a6eb9f8d6887a00dcf05fa
Renato Augusto <107155217+Argustor@users.noreply.github.com>
2023-03-09 15:21:01 -0300

    Create produto.java

---
+++ produto.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int A = myObj.nextInt();
+        int B = myObj.nextInt();
+        int PROD = A * B;
+
+        System.out.println("PROD = " + PROD);
+
+        
+    }
+}
