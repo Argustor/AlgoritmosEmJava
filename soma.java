da8ed5a192888b1b281373abe911e7dac18a7bb2
Renato Augusto <107155217+Argustor@users.noreply.github.com>
2023-03-09 15:19:23 -0300

    Create soma.java

---
+++ soma.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ //This is a comment
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int A = myObj.nextInt();
+        int B = myObj.nextInt();
+        int soma = A + B;
+        int X = soma;
+        
+        System.out.println("X = " + X);
+    
+          
+    }
+
+}
